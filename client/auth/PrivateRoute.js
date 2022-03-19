382a2e733488c4bb872c1420ac6658b4db6c8876
Islam Yusuf <yusufislam282@gmail.com>
2022-03-19 16:26:42 +0300

    Modified the PrivateRoute component by moving the navigate function call inside useEffect so that it can be called only when the component is mounted

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -1,13 +1,22 @@
-import { useNavigate, useLocation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useNavigate, useLocation} from 'react-router-dom'
 
 import auth from './auth-helper'
 
+// eslint-disable-next-line react/prop-types
 const PrivateRoute = ({ children }) => {
     const {pathname} = useLocation()
     const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(!auth.isAuthenticated()){
+            navigate('/signin', {state: {from : {pathname}},replace: true})
+        }
+    }, [])
+
     return auth.isAuthenticated() 
         ? children
-        : navigate('/signin', {state: {from : {pathname}},replace: true})
+        : (<div>Loading...</div>)
 }
 
 export default PrivateRoute
